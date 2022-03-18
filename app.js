e93d0027100c3c9700c142c558d20b7279bfd7cf
chiesa <samuelechiesa.basta@gmail.com>
2022-03-18 18:58:44 +0000

    site with jsNode

--- app.js
+++ app.js
@@ -5,29 +5,36 @@ const express = require('express')
 const app = express();
 app.use(express.static(__dirname));
 
+// 
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`, { name: 'index' });
+    res.render(`index`, { title: 'home' });
   })
 
-app.get('/strumenti.html', (req, res) => {
-  res.sendFile(`${__dirname}/strumenti.html`, {name:'index'});
+app.get('/strumenti', (req, res) => {
+  res.render(`strumenti`, {title:'immagini'});
 })
 
-app.get('/lavori.html', (req, res) => {
-    res.sendFile(`${__dirname}/lavori.html`, { name: 'index' });
+app.get('/temp', (req, res) => {
+  res.sendFile(`./temp.html`, { title: 'dove siamo' });
+})
+
+app.get('/lavori', (req, res) => {
+    res.render(`lavori`, { title: 'lavori' });
   })
 
-app.get('/dove.html', (req, res) => {
-    res.sendFile(`${__dirname}/dove.html`, { name: 'index' });
+app.get('/dove', (req, res) => {
+    res.render(`dove`, { title: 'dove siamo' });
   })
 
-app.get('/chi.html', (req, res) => {
-    res.sendFile(`${__dirname}/chi.html`, { name: 'index' });
+app.get('/chi', (req, res) => {
+    res.render(`chi`, { title: 'about me' });
   })
 
 app.use((req,res) => {
-  res.status(404).sendFile(`${__dirname}/404.html`);
+  res.status(404).render(`404.html`);
 })
 
